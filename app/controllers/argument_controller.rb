ba217ed2a8f506d8b24f204cbfd6043b5f50d485
Reid Angwin <rangwin@gmail.com>
2018-01-31 00:06:20 -0500

    Removed unused argument_controller. Using arguments_controller instead.

--- app/controllers/argument_controller.rb
+++
@@ -1,4 +0,0 @@
-class ArgumentController < ApplicationController
-  def index
-  end
-end
--- test/controllers/argument_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ArgumentControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get argument_index_url
-    assert_response :success
-  end
-
-end
