d60d7e1653fcfd2bb0fb0748d2388b12b7277ced
Reid Angwin <rangwin@gmail.com>
2018-03-10 07:12:22 -0500

    Fixed seeds.rb to create random arguments, events, and reactions. Added haikunator for heroku style random names.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,423 +6,70 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'haikunator'
 
+ $events = [
+  'Saw you in the hallway',
+  'You slammed the door',
+  'I took a shower',
+  'You started crying',
+  'I was playing music loud',
+  'You stormed out of the room',
+  'I kept working on emacs',
+  'I didn\'t say anything',
+  'You went to bed early',
+  'I went to bed early',
+  'I went to bed really late',
+  'You moved your stuff to the other room',
+  'I was hanging out with my friend',
+  'You were hanging out with your friend',
+  'Your eye started twitching involuntarily'
+]
 
-argument = Argument.create!(
-  {
-    name: 'Keep bumping into each other'
-  }
-)
+$feelings = [
+  'I felt really good',
+  'I felt alright',
+  'I felt bad',
+  'I wanted to cry',
+  'I was ecstatic',
+  'I felt conflicting emotions',
+  'I felt a bit of malaise',
+  'It just made me really hungry'
+]
 
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'We were both up late studying',
-    position: 0
-  }
-)
+def create_arguments_with_events_and_reactions(argument_count)
+  argument_count.times do
+    a = Argument.create!(name: Haikunator.haikunate(3))
+    random_event_number = rand(1..10)
+    random_event_indexes = (0..random_event_number-1).to_a.shuffle
+    random_event_number.times do
+      random_event_index = random_event_indexes.pop
+      e = Event.create!({
+        argument: a,
+        body: $events.sample,
+        position: random_event_index
+      })
+      random_feeling_number = rand(1..4)
+      random_feeling_indexes = (0..random_feeling_number-1).to_a.shuffle
+      random_feeling_number.times do
+      random_feeling_index = random_feeling_indexes.pop
+      Reaction.create!({position: random_feeling_index,
+        body: $feelings.sample,
+        user: [User.first, User.last].sample,
+        event: e
+        })
+      end
+    end
+  end
+end
 
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
+create_arguments_with_events_and_reactions(3)
 
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
+arguments = Argument.all
+events = Event.all
+reactions = Reaction.all
 
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'I tripped over your shoes strewn around the living room.',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'I tripped over your shoes strewn around the living room.',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'Already were pressured by deadlines',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'Threw your back out',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-
-argument = Argument.create!(
-  {
-    name: 'Taking too long in the bathroom'
-  }
-)
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'Really had to go and was waiting outside',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Felt powerless.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-
-argument = Argument.create!(
-  {
-    name: 'Leaving the house dirty'
-  }
-)
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'I tripped over your shoes strewn around the living room.',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'I tripped over your shoes strewn around the living room.',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
-
-event1 = Event.create!(
-  {
-    argument: argument,
-    body: 'I tripped over your shoes strewn around the living room.',
-    position: 0
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 0,
-    body: 'Really offended that you weren\'t looking where you were going',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 1,
-    body: 'Felt good too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 2,
-    body: 'Felt bad too.',
-    user: User.first
-  }
-)
-
-Reaction.create!(
-  {
-    event: event1,
-    position: 3,
-    body: 'Some malaise perhaps.',
-    user: User.first
-  }
-)
+puts "Seed finished"
+puts "#{arguments.count} arguments created"
+puts "#{events.count} events created"
+puts "#{reactions.count} reactions created"
